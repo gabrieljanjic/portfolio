4f691b9548eab85a8e42fb7c6e4aeb67560da834
Gabriel Janjic <janjicgabriel1@gmail.com>
2026-01-14 22:08:42 +0100

    Added next.js logo

--- src/data/techStackData.tsx
+++ src/data/techStackData.tsx
@@ -8,6 +8,7 @@ import TypeScriptLogo from "../imgs/typescript.png";
 import MongoDbLogo from "../imgs/mongo-db.png";
 import MySqlLogo from "../imgs/my-sql.png";
 import NodeJsLogo from "../imgs/node-js.png";
+import NextJsLogo from "../imgs/next-js.png";
 
 type TechStackItem = {
   id: number;
@@ -18,53 +19,59 @@ type TechStackItem = {
 const techStackData: TechStackItem[] = [
   {
     id: 1,
-    name: "HTML",
-    path: HtmlLogo,
+    name: "Next.js",
+    path: NextJsLogo,
   },
   {
     id: 2,
-    name: "CSS",
-    path: CssLogo,
-  },
-  {
-    id: 3,
-    name: "JavaScript",
-    path: JavaScriptLogo,
-  },
-  {
-    id: 4,
     name: "React",
     path: ReactLogo,
   },
   {
-    id: 5,
+    id: 3,
     name: "TypeScript",
     path: TypeScriptLogo,
   },
   {
-    id: 6,
-    name: "Bootstrap.js",
-    path: BootstrapLogo,
+    id: 4,
+    name: "JavaScript",
+    path: JavaScriptLogo,
   },
   {
-    id: 7,
-    name: "Tailwind",
-    path: TailwindLogo,
-  },
-  {
-    id: 8,
+    id: 5,
     name: "Node.js",
     path: NodeJsLogo,
   },
   {
-    id: 9,
+    id: 6,
+    name: "MongoDB",
+    path: MongoDbLogo,
+  },
+  {
+    id: 7,
     name: "MySQL",
     path: MySqlLogo,
   },
   {
+    id: 8,
+    name: "Tailwind",
+    path: TailwindLogo,
+  },
+
+  {
+    id: 9,
+    name: "Bootstrap.js",
+    path: BootstrapLogo,
+  },
+  {
     id: 10,
-    name: "MongoDB",
-    path: MongoDbLogo,
+    name: "HTML",
+    path: HtmlLogo,
+  },
+  {
+    id: 11,
+    name: "CSS",
+    path: CssLogo,
   },
 ];
 
