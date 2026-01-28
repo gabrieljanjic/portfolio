d3802d4faec12af75f65e966824572082e399405
Gabriel Janjic <janjicgabriel1@gmail.com>
2026-01-28 22:56:18 +0100

    Added new search

--- src/data/projectsData.tsx
+++ src/data/projectsData.tsx
@@ -1,12 +1,14 @@
 import ReactLogo from "../imgs/react.png";
-import BootstrapLogo from "../imgs/bootstrap.png";
 import CssLogo from "../imgs/css.png";
-import HtmlLogo from "../imgs/html.png";
 import JavaScriptLogo from "../imgs/javascript.png";
+import TypeScriptLogo from "../imgs/typescript.png";
 import MongoDbLogo from "../imgs/mongo-db.png";
 import NodeJsLogo from "../imgs/node-js.png";
+import TailwindLogo from "../imgs/tailwind.png";
+import NextJsLogo from "../imgs/next-js.png";
+
 import TravelHubTabletImg from "../imgs/project-imgs/travel-hub-tablet-img.webp";
-import BookingAppTabletImg from "../imgs/project-imgs/booking-app-tablet-img.webp";
+import MovioTabletImg from "../imgs/project-imgs/movio-app-tablet-img.webp";
 
 type TechnologyItem = {
   id: number;
@@ -31,30 +33,31 @@ const ReactTechnology: TechnologyItem = {
   logo: ReactLogo,
   backgroundColor: "#0d6efd",
 };
-const BootstrapTechnology: TechnologyItem = {
-  id: 2,
-  name: "Bootstrap",
-  logo: BootstrapLogo,
-  backgroundColor: "#6610f2",
-};
 
+const TailwindTechnology: TechnologyItem = {
+  id: 3,
+  name: "Tailwind CSS",
+  logo: TailwindLogo,
+  backgroundColor: "#1e40af",
+};
 const CssTechnology: TechnologyItem = {
   id: 4,
   name: "CSS",
   logo: CssLogo,
   backgroundColor: "#1e40af",
 };
-const HtmlTechnology: TechnologyItem = {
-  id: 5,
-  name: "HTML",
-  logo: HtmlLogo,
-  backgroundColor: "#b45309",
-};
+
 const JavaScriptTechnology: TechnologyItem = {
   id: 6,
   name: "JavaScript",
   logo: JavaScriptLogo,
   backgroundColor: "#f59e0b",
+};
+const TypeScriptTechnology: TechnologyItem = {
+  id: 7,
+  name: "TypeScript",
+  logo: TypeScriptLogo,
+  backgroundColor: "#0600af",
 };
 
 const MongoDbTechnology: TechnologyItem = {
@@ -71,9 +74,32 @@ const NodeJsTechnology: TechnologyItem = {
   backgroundColor: "#14532d",
 };
 
+const NextJsTechnology: TechnologyItem = {
+  id: 11,
+  name: "Next.js",
+  logo: NextJsLogo,
+  backgroundColor: "#6d6d6d",
+};
+
 const projectData: ProjectItem[] = [
   {
     id: 1,
+    name: "Movio",
+    description:
+      "A full-stack social platform for movie and TV series enthusiasts. Discover trending content, connect with other film lovers, and share your thoughts through a personalized feed.",
+    technology: [
+      NextJsTechnology,
+      ReactTechnology,
+      TypeScriptTechnology,
+      MongoDbTechnology,
+      TailwindTechnology,
+    ],
+    githubLink: "https://github.com/gabrieljanjic/movio",
+    pageLink: "https://movio-delta.vercel.app/",
+    img: MovioTabletImg,
+  },
+  {
+    id: 2,
     name: "Travel Hub",
     description:
       "Travel Hub is a full-stack web application for managing airlines, airports, routes, and countries with an interactive map. It features real-time dashboards, CRUD operations, route distance calculations, and secure user authentication.",
@@ -87,23 +113,6 @@ const projectData: ProjectItem[] = [
     githubLink: "https://github.com/gabrieljanjic/travel-hub",
     pageLink: "https://travel-hub-indol.vercel.app/dashboard",
     img: TravelHubTabletImg,
-  },
-  {
-    id: 2,
-    name: "Booking App",
-    description:
-      "Booking App is a full-stack web application for managing hotels and reservations, allowing users to browse hotels, create and manage bookings, search and filter listings, and securely authenticate with JWT tokens and HTTP-only cookies, with geolocation provided by Google Maps API.",
-    technology: [
-      HtmlTechnology,
-      CssTechnology,
-      BootstrapTechnology,
-      JavaScriptTechnology,
-      NodeJsTechnology,
-      MongoDbTechnology,
-    ],
-    githubLink: "https://github.com/gabrieljanjic/booking-app",
-    pageLink: "https://booking-app-bmps.onrender.com/",
-    img: BookingAppTabletImg,
   },
 ];
 
--- src/data/workExperience.tsx
+++ src/data/workExperience.tsx
@@ -12,7 +12,7 @@ const workExperienceData: WorkExperienceItem[] = [
   {
     id: 1,
     position: "Full-stack developer",
-    company: "Ergo-net",
+    company: "Ergo-net d.o.o",
     description:
       "Worked 8 months at an accounting company, maintaining and improving the client-facing application. Developed new features, fixed bugs, designed UI, and created APIs using HTML, CSS, JavaScript, PHP (Laravel), and SQL, both independently and in a team.",
     start: "May 2025",
